f4301aef9cecbcb393d76ef52c0986e4b568cd9c
Estefanía Leal Martínez <78847434+EstefaniaLM@users.noreply.github.com>
2021-02-27 20:22:21 +0100

    Delete Github directory

--- Github/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"CribaJavaDoc"},{"p":"<Unnamed>","l":"CribaOriginal"},{"p":"<Unnamed>","l":"CribaRefactorizado"}]
