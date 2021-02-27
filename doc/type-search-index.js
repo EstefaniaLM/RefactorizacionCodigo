e8fa83e50a00e6b406cbe82b513e959709edf119
Estefanía Leal Martínez <78847434+EstefaniaLM@users.noreply.github.com>
2021-02-27 20:18:20 +0100

    Add files via upload

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"CribaJavaDoc"},{"p":"<Unnamed>","l":"CribaOriginal"},{"p":"<Unnamed>","l":"CribaRefactorizado"}]
